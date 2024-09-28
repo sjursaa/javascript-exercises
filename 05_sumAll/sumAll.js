dcd6096a7b7efa9a5fe46c2440b2696b76fc50cf
Sjur <sjursaa@gmail.com>
2024-09-28 21:40:30 +0200

    sumAll pass test 1

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
+const sumAll = function () {
+  const start = arguments[0];
+  const stop = arguments[1];
+  let sum = 0;
 
+  for (let i = start; i <= stop; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
