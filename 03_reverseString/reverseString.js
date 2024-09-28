316ab07c3b6f38d3a94124c8dd27bfaff9d88831
Sjur <sjursaa@gmail.com>
2024-09-28 17:44:20 +0200

    solve reverse string, all 4 tests passed.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  let element = "";
+  for (let index = string.length - 1; index >= 0; index--) {
+    element += string[index];
+  }
+  return element;
 };
 
 // Do not edit below this line
