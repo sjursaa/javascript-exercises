d5f10cfcc069ebd59440ead38630f5da38e0347c
Sjur <sjursaa@gmail.com>
2024-09-28 21:33:38 +0200

    removeFromArray 3/8 with pop

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -2,16 +2,41 @@ const removeFromArray = function (array, removeItem) {
   let returnArray = [];
   let removeArray = [];
   removeArray.push(removeItem);
+  console.log("OG-Array " + array);
+  console.log("OG remove list " + removeItem);
+  console.log("Remove this " + removeArray);
 
-  for (let i = 0; i < array.length; i++) {
-    for (let j = 0; j < removeArray.length; j++) {
-      if (array[i] != removeArray[j]) {
-        returnArray.push(array[i]);
+  // solution 1
+
+  // TODO sort removeArray
+  removeArray.sort();
+  console.log("Sorted removeArray " + removeArray);
+
+  // TODO solve for unsorted arrays.
+
+  while (removeArray.length != 0) {
+    for (let i = 0; i < array.length; i++) {
+      for (let j = 0; j < removeArray.length; j++) {
+        if (array[i] !== removeArray[j]) {
+          returnArray.push(array[i]);
+        }
       }
+      //console.log(returnArray[i]);
     }
-    console.log(returnArray[i]);
+    removeArray.pop();
   }
-  console.log(returnArray);
+
+  // must work with strings and numbers
+  // might need sorting of the removeArray
+
+  /*
+  // solution 2
+  for (let i = 0; i < removeArray.length; i++) {
+    //array.splice(removeArray[i]);
+    array.slice();
+  }
+*/
+  console.log("Result post alg: " + array);
   return returnArray;
 };
 
