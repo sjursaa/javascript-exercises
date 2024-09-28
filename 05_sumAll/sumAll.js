20cdf4b23a9c36c8942a61ecd4b414f5dec294ac
Sjur <sjursaa@gmail.com>
2024-09-28 22:04:51 +0200

    sumAll pass all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,32 @@
 const sumAll = function () {
-  const start = arguments[0];
-  const stop = arguments[1];
+  let start;
+  let stop;
+
+  // check user input is number
+  if (typeof arguments[0] != "number" || typeof arguments[1] != "number") {
+    return "ERROR";
+  }
+
+  // assign smaller argument to start/bigger to stop
+  if (arguments[0] < arguments[1]) {
+    start = arguments[0];
+    stop = arguments[1];
+  } else if (arguments[0] >= arguments[1]) {
+    start = arguments[1];
+    stop = arguments[0];
+  } else {
+    return "ERROR";
+  }
+
+  if (start <= 0 || stop <= 0) {
+    return "ERROR";
+  }
+
+  // check if number is int or float
+  if (start % 1 !== 0 || stop % 1 !== 0) {
+    return "ERROR";
+  }
+
   let sum = 0;
 
   for (let i = start; i <= stop; i++) {
