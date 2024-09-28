fd3c92e8f527fbc0b58bb1c4863fa96de6a80979
Sjur <sjursaa@gmail.com>
2024-09-28 22:48:38 +0200

    tempConversion passed all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function () {
+  // TODO, round to one decimal
+  // TODO, fix conversion for negative values
+  let tempFar = arguments[0];
+  let tempCel = (tempFar - 32) * (5 / 9);
+  return +tempCel.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function () {
+  // TODO, round to one decimal
+  // TODO, fix conversion for negative values
+  let tempCel = arguments[0];
+  let tempFar = tempCel * (9 / 5) + 32;
+  return +tempFar.toFixed(1);
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
