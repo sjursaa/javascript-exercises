897f56fa1d9425373a80d7de378b86d0988fc844
Sjur <sjursaa@gmail.com>
2024-09-28 19:00:24 +0200

    basic solution with 2 loops passes 3 of 8 tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, removeItem) {
+  let returnArray = [];
+  let removeArray = [];
+  removeArray.push(removeItem);
+
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < removeArray.length; j++) {
+      if (array[i] != removeArray[j]) {
+        returnArray.push(array[i]);
+      }
+    }
+    console.log(returnArray[i]);
+  }
+  console.log(returnArray);
+  return returnArray;
 };
 
 // Do not edit below this line
