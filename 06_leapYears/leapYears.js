4c7250682cb4233ca712c742d29680871009b3a1
Sjur <sjursaa@gmail.com>
2024-09-28 22:29:37 +0200

    leapYears passed 4/6 tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
+const leapYears = function () {
+  const year = arguments[0];
 
+  if (year % 4 == 0) {
+    return true;
+  } else if (year >= 400 && year % 400 == 0) {
+    return true;
+  }
+  return false;
 };
 
 // Do not edit below this line
